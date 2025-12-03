526390465a772ae97cbb0efbf9af3fad28abc87b
Harman <harmandeepsingh7@student.mitt.ca>
2025-12-03 12:30:48 -0600

    Added a js code for map to show current live location

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,3 +9,48 @@ const map = new mapboxgl.Map({
     zoom: 16,
     pitch: 40
 });
+
+const marker = new mapboxgl.Marker({
+    color: '#44403eff'   
+});
+
+const options = {
+    enableHighAccuracy: true
+};
+
+function getLocation(position) {
+    let { latitude, longitude } = position.coords;
+    map.setCenter([longitude, latitude]);
+    marker.setLngLat([longitude, latitude]).addTo(map);
+    console.log(`Latitude: ${latitude} | Longitude: ${longitude}`);
+}
+
+
+function errorHandler() {
+    console.log('Unable to retrieve your location');
+}
+
+function disableMapControls() {
+    map.dragPan.disable();
+    map.keyboard.disable();
+    map.scrollZoom.disable();
+    map.doubleClickZoom.disable();
+    map.touchZoomRotate.disable();
+}
+
+
+function startTracking() {
+    if ('geolocation' in navigator) {
+        navigator.geolocation.watchPosition(
+            getLocation,
+            errorHandler,
+            options
+        );
+
+        disableMapControls();
+    } else {
+        console.log('Geolocation is not supported by your browser.');
+    }
+}
+
+startTracking();
